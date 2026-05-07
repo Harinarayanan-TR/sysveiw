d4fb0b5bd367d53dc340e9292d17984e61792af9
Harinarayanan-TR <error40404.github@gmail.com>
2026-05-07 16:32:06 +0530

    Delete server.js
    this is the v1.1.1 version. go to v1.1.1 branch to veiw v1.1.1 files.

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("updates"));
-
-app.listen(3000, () => {
-  console.log("OTA server running on http://localhost:3000");
-});
